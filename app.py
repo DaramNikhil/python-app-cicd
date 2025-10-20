7ffdcf3bcad3be89fd50d69475b4b51428c575fa
Nikhil Daram <nikhildaram51@gmail.com>
2025-10-20 03:24:59 +0000

    update code

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 def add(a: int, b: int):
     return a + b
 
+def substract(a: int, b: int):
+    return a - b
+
 
 if __name__ == "__main__":
     print(add(5, 3))
+    print(substract(5, 3))
--- test_app.py
+++ test_app.py
@@ -1,5 +1,9 @@
-from app import add
+from app import add, substract
 
 def test_add():
     assert add(5, 3) == 8
     assert add(-1, 1) == 0
+
+def test_substract():
+    assert substract(5, 1) == 2
+    assert substract(5 - 5) == 0
